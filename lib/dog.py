b3622257f2de970290be82eadc4b5cc0b81136fa
Grace <gracemwangi846@gmail.com>
2025-01-22 14:57:00 +0300

    First commit

--- lib/dog.py
+++ lib/dog.py
@@ -10,6 +10,30 @@ APPROVED_BREEDS = [
     "Pug",
     "Pointer"
 ]
-
 class Dog:
-    pass
+    def __init__(self, name="Default", breed = None):
+        self.name=name  
+        self.breed = breed
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value  
+        else:
+            print("Name must be string between 1 and 25 characters.")  # Print error
+            self._name = "Default"  
+    def get_name(self):
+        return self._name  
+
+    def set_breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None
+
+    def get_breed(self):
+        return self._breed       
+
+
+    name = property(get_name, set_name)
+    #breed = property (get_breed , set_breed)
